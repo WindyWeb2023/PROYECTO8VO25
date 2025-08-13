9224eca136ac568922e4d3d818a5dd9159e34cc7
WindyWeb2023 <minegocioweb.rib@gmail.com>
2025-08-13 10:00:43 -0400

    Data base initial

---
+++ Proyecto8vo25/Proyecto.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto.Shared.Entidad;
+
+namespace Proyecto.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        { 
+        
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Usuario>().HasIndex(u => u.Carnet).IsUnique();
+
+        }
+
+    }
+}
---
+++ Proyecto8vo25/Proyecto.API/Migrations/20250813135356_ProyectoDB.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProyectoDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Carnet = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_Carnet",
+                table: "Usuarios",
+                column: "Carnet",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}

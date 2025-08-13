fad4fe789bd4cafc8c085943cf0ba0dcf4cc6df6
WindyWeb2023 <minegocioweb.rib@gmail.com>
2025-08-13 15:19:22 -0400

    CONTROLADOR 1

---
+++ Proyecto8vo25/Proyecto.API/Controllers/UsariosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto.API.Data;
+
+namespace Proyecto.API.Controllers
+{
+    [ApiController]
+    [Route("/api/usuarios")]
+    public class UsariosController : ControllerBase
+    {
+        //le decimos que sea solo lectura los atributos de  Datacontext
+        private readonly DataContext _dataContext;
+        public UsariosController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await 
+                _dataContext.Usuarios.ToListAsync());
+        }
+
+    }
+}

9bf63f2c8e7dcd2e86525d2b72ccf220a286ac10
WindyWeb2023 <minegocioweb.rib@gmail.com>
2025-08-20 13:59:25 -0400

    Primer get y post

--- Proyecto8vo25/Proyecto.API/Program.cs
+++ Proyecto8vo25/Proyecto.API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>
-    (x => x.UseSqlServer("name=Conexion"));
+    (x => x.UseSqlServer("name=Conexion")); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
